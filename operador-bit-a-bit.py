d5c324b8fb48f84331be669b5e8fca762e5ca9bc
Marina Gomes <maarinagomes1@gmail.com>
2023-05-13 22:53:49 -0300

    Create operador-bit-a-bit.py
    execicio da unidade

---
+++ operador-bit-a-bit.py
@@ -0,0 +1,28 @@
+#Exercicio1
+var = 17
+var_right = var >> 1
+var_left = var << 2
+print (var, var_left, var_right)
+
+#Operador de bits logicos unid 3
+
+#Exercicio2
+x = 1
+y = 0
+ 
+z = ((x == y) and (x == y)) or not(x == y)
+print(not(z))
+
+#Exercicio3
+x = 4
+y = 1
+ 
+a = x & y
+b = x | y
+c = ~x  # tricky!
+d = x ^ 5
+e = x >> 2
+f = x << 2
+
+print(a, b, c, d, e, f)
+
